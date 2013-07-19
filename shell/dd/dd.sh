7d994ea4db789626bf94688b291352b64d96688a
liutailin <jackliu618@gmail.com>
2013-07-19 13:06:30 +0800

    add 原始凭证处理.md

--- shell/dd/dd.sh
+++ shell/dd/dd.sh
@@ -8,7 +8,11 @@ dd if=/dev/sda1_part.img of=/dev/sda1 bs=512    #  restore
 #    制作光盘镜像
 dd if=/dev/cdrom of=/data/cdrom.iso
 
+dd if=/ubunto.iso of=/dev/sdb
+
 #    挂载镜像文件
 mkdir /mnt/img
 mount -o loop file.img /mnt/img
 
+# 卸载
+umount
