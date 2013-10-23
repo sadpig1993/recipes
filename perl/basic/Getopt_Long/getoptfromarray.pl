26bb4de86c5d4d951a6171983a3b9bdecf7841fc
liutailin <jackliu618@gmail.com>
2013-10-23 16:43:03 +0800

    add DateTime demo and Net_FTP demo

--- perl/basic/Getopt_Long/getoptfromarray.pl
+++ perl/basic/Getopt_Long/getoptfromarray.pl
@@ -5,7 +5,8 @@ use Getopt::Long qw/GetOptionsFromArray/;
 use Data::Dump;
 
 my $length ;
-my $ret = GetOptionsFromArray( \@{[@ARGV]}, 'length|l=s' => \$length );
+#my $ret = GetOptionsFromArray( \@{[@ARGV]}, 'length|l=s' => \$length );
+my $ret = GetOptionsFromArray( \@ARGV, 'length|l=s' => \$length );
 
 # perl getopt8.pl -length 1000
 print <<EOF;
