c385ee086f1b070d360da5ab62948fc2aee9eea1
liutailin <jackliu618@gmail.com>
2013-10-28 15:35:39 +0800

    add op directory

--- perl/basic/op/sort.pl
+++ perl/basic/op/sort.pl
@@ -6,17 +6,21 @@ use warnings;
 my @files ;
 @files = qw/hello ha boy/;
 my @articles ;
- # sort lexically
+
+# sort lexically 词法排序
 @articles = sort @files;
 print "@articles\n" ;
 
-# same thing, but with explicit sort routine
+# same thing, but with explicit(明显) sort routine
 @articles = sort {$a cmp $b} @files;
 print "@articles\n";
 
- # now case-insensitively
+# now case-insensitively 忽略大小写 一律大写
 @articles = sort {uc($a) cmp uc($b)} @files;
-print "@articles\n";
+print "uc sort :@articles\n";
+
+@articles = sort {lc($a) cmp lc($b)} @files;
+print "lc sort :@articles\n";
 
 # same thing in reversed order
 @articles = sort {$b cmp $a} @files ;
