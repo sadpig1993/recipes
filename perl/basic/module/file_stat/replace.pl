c3ed69b872c2501ae4d857fbc0a8267160f95b99
liutailin <jackliu618@gmail.com>
2013-11-26 19:22:12 +0800

    add some perl demo

--- perl/basic/module/file_stat/replace.pl
+++ perl/basic/module/file_stat/replace.pl
@@ -8,6 +8,21 @@ use File::stat;
 use IO::File;
 use Carp;
 
+=comment
+bip表有blob字段，每次维护太麻烦，特整理此脚本，结合export.sh import.sh做bip表的数据维护。
+1. export.sh 导出bip数据的时候，逐条导出成del文件对对应的lob文件
+2. delete.pl 删除没有bip数据的del文件
+3. 运行此程序修改del文件中的对lob文件大小的描述(lob文件有修改的时候)
+4. import.sh 将修改后的lob文件和del文件导入到bip表
+
+此脚本的思路：
+1. 把有数据的文件重定向到allfiles文件
+2. 从allfiles文件中逐个遍历del文件及对应的lob文件的大小
+3. 生成一个临时文件，把其中的lob文件的字节的大小进行更改
+4. 用生成的临时文件覆盖原来的对应的del文件
+
+=cut
+
 my $rfh = IO::File->new("<allfiles");
 if ($@) {
     confess "cannot open, errmsg is $@\n";
