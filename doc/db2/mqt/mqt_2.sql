8325337794058b75f2aba11808e0506fbe386589
liutailin <jackliu618@gmail.com>
2013-12-26 13:51:20 +0800

    update mqt_2.sql

--- doc/db2/mqt/mqt_2.sql
+++ doc/db2/mqt/mqt_2.sql
@@ -17,7 +17,11 @@ data initially deferred refresh deferred;
 set integrity for sum_wlzj_yfbf materialized query immediate unchecked;
 
 
--- 创建MQT表sum_wlzj_yfbf对应的登台表 staging  此时有个警告 SQLSTATE 01504   这个警告该怎么解决呢？？？
+-- 创建MQT表sum_wlzj_yfbf对应的登台表 staging  此时有个警告 SQLSTATE 01504   这个警告该怎么解决呢？？？ 在Zark.pm里面的
+-- _add_mqt_stg方法里 忽略此警告      
+-- # 忽略警告 01504 
+-- local $self->{dbh}->{RaiseError};
+
 create table stg_sum_wlzj_yfbf for sum_wlzj_yfbf propagate immediate ;
 
 set integrity for stg_sum_wlzj_yfbf staging immediate unchecked;
