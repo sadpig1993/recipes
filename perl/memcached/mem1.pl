8a79c54a819e431d3d779bf5e2a23dc2c29a579b
liutailin <jackliu618@gmail.com>
2014-02-11 11:37:31 +0800

    add some demo and doc

--- perl/memcached/mem1.pl
+++ perl/memcached/mem1.pl
@@ -7,7 +7,7 @@ use strict;
 use warnings;
 
 my $memd = new Cache::Memcached {
-    'servers'            => 'localhost:11211',
+    'servers'            => [ "127.0.0.1:11211" ],
     'debug'              => 0,
     'compress_threshold' => 10_000,
 };
@@ -15,5 +15,19 @@ my $memd = new Cache::Memcached {
 $memd->set( "my_key", "Some value" );
 $memd->set( "object_key", { 'complex' => [ "object", 2, 4 ] } );
 
+my $href; # hash ref
 
 Data::Dump->dump($memd->get("my_key"));
+Data::Dump->dump($memd->get("object_key"));
+$href = $memd->get_multi("object_key");
+Data::Dump->dump($href);
+
+Data::Dump->dump($href->{object_key}->{complex}->[0]); 
+Data::Dump->dump($href->{object_key}->{complex}->[1]); 
+Data::Dump->dump($href->{object_key}->{complex}->[2]); 
+
+
+my $val = $memd->get("object_key");
+print $val->{complex}->[0] . "\n";
+print $val->{complex}->[1] . "\n";
+print $val->{complex}->[2] . "\n";
--- perl/memcached/mem3.pl
+++ perl/memcached/mem3.pl
@@ -9,36 +9,44 @@ my $dbuser   = 'db2inst';
 my $dbpasswd = 'db2inst';
 my $db       = 'zdb_dev';
 my $dsn      = "dbi:DB2:$db";
-my $dbh =
-  DBI->connect( $dsn, $dbuser, $dbpasswd,
-    { PrintError => 1, RaiseError => 0 } );
+
+my $dbh = DBI->connect(
+    $dsn, $dbuser, $dbpasswd,
+    {
+        PrintError => 1,
+        RaiseError => 0
+    }
+);
 
 my $test_id = 4;
 
-my $memd    = new Cache::Memcached {
+my $memd = new Cache::Memcached {
     'servers'            => [ "127.0.0.1:11211" ],
     'debug'              => 0,
     'compress_threshold' => 10_000,
 };
+
 my $val;
 $val = init_object($test_id);
-print "init " . Data::Dump->dump($val);
+print "init " . Data::Dump->dump($val) . "\n";
 
-repl_object($test_id);
+$val = repl_object($test_id);
+print "repl  " . Data::Dump->dump($val) . "\n";
+
 $val = get_object($test_id);
-print "get " . Data::Dump->dump($val);
+print "get value " . Data::Dump->dump($val) . "\n";
 
-$val = incr_object($test_id);
-print "incr1 " . Data::Dump->dump($val);
-
-$val = incr_object($test_id);
-print "incr2 " . Data::Dump->dump($val);
+# $val = incr_object($test_id);
+# print "incr1 " . Data::Dump->dump($val) . "\n";
+# 
+# $val = incr_object($test_id);
+# print "incr2 " . Data::Dump->dump($val) . "\n";
 
 if ( del_object($test_id) ) {
     my $val = get_object($test_id);
-    print "del " . Data::Dump->dump($val);
+    print "del " . Data::Dump->dump($val) . "\n";
 }
-print "stats " . Data::Dump->dump($memd->stats('misc'));
+print "stats " . Data::Dump->dump( $memd->stats('misc') ) . "\n";
 $memd->disconnect_all;
 
 #----------------
@@ -67,9 +75,10 @@ sub del_object {
 
 sub repl_object {
     my $foo_id = shift;
-    my $query  = "select id, name from test where id=4";
+    my $query  = "select id, name from test where id=$foo_id";
     my $obj    = $dbh->selectrow_hashref($query);
     $memd->replace( "foo:$foo_id", $obj );
+    return $obj;
 }
 
 sub incr_object {
